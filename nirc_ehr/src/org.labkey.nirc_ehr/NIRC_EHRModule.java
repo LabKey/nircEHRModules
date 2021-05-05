b96da94348b25203dae151a47fd39e6cdb6eb38f
labkey-ankurj <ankurj@labkey.com>
2021-05-05 11:41:31 -0700

    Initial setup of nirc ehr module

---
+++ nirc_ehr/src/org.labkey.nirc_ehr/NIRC_EHRModule.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2021 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.labkey.nirc_ehr;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.labkey.api.ehr.EHRService;
+import org.labkey.api.ldk.ExtendedSimpleModule;
+import org.labkey.api.module.ModuleContext;
+import org.labkey.api.view.WebPartFactory;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class NIRC_EHRModule extends ExtendedSimpleModule
+{
+    public static final String NAME = "NIRC_EHR";
+
+    @Override
+    public String getName()
+    {
+        return NAME;
+    }
+
+    @Override
+    public @Nullable Double getSchemaVersion()
+    {
+        return 21.000;
+    }
+
+    @Override
+    public boolean hasScripts()
+    {
+        return false;
+    }
+
+    @Override
+    @NotNull
+    protected Collection<WebPartFactory> createWebPartFactories()
+    {
+        return Collections.emptyList();
+    }
+
+    @Override
+    protected void init()
+    {
+        addController(NIRC_EHRController.NAME, NIRC_EHRController.class);
+    }
+
+    @Override
+    protected void doStartupAfterSpringConfig(ModuleContext moduleContext)
+    {
+        EHRService ehrService = EHRService.get();
+        ehrService.registerModule(this);
+    }
+}
