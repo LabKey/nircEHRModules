ba7f3b3d1dc408f0da1e97a1ee4662a70c90a32f
labkey-ankurj <ankurj@labkey.com>
2021-07-08 16:47:41 -0700

    basic demographics ETL

---
+++ nirc_ehr/resources/queries/dbo/q_demographics.sql
@@ -0,0 +1,10 @@
+
+SELECT ANIMAL_ID_NUMBER AS Id,
+       BIRTH_DATE,
+       GENDER_ID AS gender,
+       DESCRIPTION AS description,
+       TERMINATION_REASON_ID AS termination,
+       IMPLANT_NUMBER AS implantNumber,
+       VENDOR_ANIMAL_NUMBER AS vendorNumber,
+       ACTIVE_YN AS status
+FROM Animal
