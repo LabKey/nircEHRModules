2e57dad2168bb5dd6fd0981bfc5b755f8555f664
Ankur Juneja <ankurj@labkey.com>
2021-07-14 12:54:25 -0700

    Item 9014 - Initial ETLs setup (#6)

---
+++ nirc_ehr/resources/queries/dbo/q_species_codes.sql
@@ -0,0 +1,11 @@
+
+SELECT
+    SPECIES_ID AS code,
+    SPEC_NAME AS scientific_name,
+    SPEC_DESC AS description,
+    SPECIES_GROUP_ID as species,
+    CASE ACTIVE_YN
+        WHEN 'Y' THEN NULL
+        WHEN 'N' THEN '1/1/1970'
+        END AS dateDisabled
+FROM SPECIES
