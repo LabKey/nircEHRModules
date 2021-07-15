5370b21c84b8ed0d5bddb5bd982bf5f35bcd04ca
labkey-ankurj <ankurj@labkey.com>
2021-07-14 17:27:47 -0700

    move dam sire outside acquired date check - cr feedback

--- nirc_ehr/resources/queries/study/arrival.js
+++ nirc_ehr/resources/queries/study/arrival.js
@@ -4,20 +4,22 @@ EHR.Server.TriggerManager.registerHandlerForQuery(EHR.Server.TriggerManager.Even
     // if born at center acq date and birth date is same (row.description contains ACQ date)
     // if brought from outside aqc date and birth date may differ
 
-    if (row.source === 'Alternate' && row.acqDateText) {
-        let acqDateText = row.acqDateText.split(':')[1]; // format - ACQ: 24 Sep 2008
-        if (!acqDateText) {
-            acqDateText = row.acqDateText.split(';')[1]; // at least one value is there with ';'
-        }
-        if (acqDateText) {
-            let acqDate = new Date(acqDateText.trim());
-            if (acqDate !== row.birth) {
-                row.date = acqDate;
+    if (row.source === 'Alternate' ) {
+        if (row.acqDateText) {
+            let acqDateText = row.acqDateText.split(':')[1]; // format - ACQ: 24 Sep 2008
+            if (!acqDateText) {
+                acqDateText = row.acqDateText.split(';')[1]; // at least one value is there with ';'
             }
-            else {
-                // investigate to see if any row exists
-                console.log("born at center - ", row.Id);
-                row.date = row.birth;
+            if (acqDateText) {
+                let acqDate = new Date(acqDateText.trim());
+                if (acqDate !== row.birth) {
+                    row.date = acqDate;
+                }
+                else {
+                    // investigate to see if any row exists
+                    console.log("born at center - ", row.Id);
+                    row.date = row.birth;
+                }
             }
         }
 
