d53c9a52f5cd9fb005a76dbef85bbc2af5d1c68d
labkey-ankurj <ankurj@labkey.com>
2021-07-15 23:46:30 -0700

    Item 9014 - departure ETL

--- nirc_ehr/resources/queries/dbo/q_arrival.sql
+++ nirc_ehr/resources/queries/dbo/q_arrival.sql
@@ -8,7 +8,7 @@ SELECT anm.ANIMAL_ID_NUMBER AS Id,
        alt.DESCRIPTION AS acqDateText,
        'Alternate'          AS source
 FROM  ALTERNATE alt
-          LEFT JOIN ANIMAL anm ON alt.ANIMAL_ID = anm.ANIMAL_ID
+LEFT JOIN ANIMAL anm ON alt.ANIMAL_ID = anm.ANIMAL_ID
 WHERE alt.ALTERNATE_TYPE_ID = 7 -- 'Dam/Sire/Acq'
   AND anm.ANIMAL_ID_NUMBER NOT LIKE 'A%' -- Animal born in centers are pre-appended with A's (in animal Id)
   AND alt.DESCRIPTION IS NOT NULL
@@ -24,7 +24,7 @@ SELECT anm.ANIMAL_ID_NUMBER  AS Id,
        NULL                  AS acqDateText,
        'Animal Event'        AS source
 FROM ANIMAL_EVENT anmEvt
-         LEFT JOIN ANIMAL anm ON anmEvt.ANIMAL_ID = anm.ANIMAL_ID
+LEFT JOIN ANIMAL anm ON anmEvt.ANIMAL_ID = anm.ANIMAL_ID
 WHERE EVENT_ID IN (SELECT EVENT_ID FROM EVENT WHERE NAME LIKE 'Lab Transfer Fr%')
   AND anm.ANIMAL_ID NOT IN (SELECT ANIMAL_ID FROM ALTERNATE WHERE ALTERNATE_TYPE_ID = 7)
 
