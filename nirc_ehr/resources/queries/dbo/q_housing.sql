813b1a7b0c4aea812ca7f85f38ea75eaac9d8af6
labkey-ankurj <ankurj@labkey.com>
2021-07-30 13:56:56 -0700

    utilize auto reload study and rerun etl functionality using upgrade script

--- nirc_ehr/resources/queries/dbo/q_housing.sql
+++ nirc_ehr/resources/queries/dbo/q_housing.sql
@@ -1,7 +1,8 @@
 
 SELECT anm.ANIMAL_ID_NUMBER AS Id,
        CAST(anmEvt.EVENT_DATETIME AS TIMESTAMP) AS transferDate,
-       anmCmt.TEXT AS locationTransferText
+       anmCmt.TEXT AS remark,
+       anmEvt.LOCATION_ID AS location
 FROM ANIMAL_EVENT anmEvt
 LEFT JOIN ANIMAL anm ON anmEvt.ANIMAL_ID = anm.ANIMAL_ID
 LEFT JOIN ANIMAL_EVENT_COMMENT anmCmt ON anmEvt.ANIMAL_EVENT_ID = anmCmt.ANIMAL_EVENT_ID
--- nirc_ehr/src/org/labkey/nirc_ehr/NIRC_EHRModule.java
+++ nirc_ehr/src/org/labkey/nirc_ehr/NIRC_EHRModule.java
@@ -44,7 +44,7 @@ public class NIRC_EHRModule extends ExtendedSimpleModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 21.001;
+        return 21.002;
     }
 
     @Override
