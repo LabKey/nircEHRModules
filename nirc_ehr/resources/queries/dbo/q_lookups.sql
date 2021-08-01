448b08b93d2bdbffabb148cc85af8080bf4bd844
labkey-ankurj <ankurj@labkey.com>
2021-08-01 14:46:28 -0700

    protocol lookups

--- nirc_ehr/resources/queries/dbo/q_lookups.sql
+++ nirc_ehr/resources/queries/dbo/q_lookups.sql
@@ -25,3 +25,57 @@ WHEN 'N' THEN '1/1/1970'
 END AS date_disabled,
 NULL AS category
 FROM TERMINATION_REASON
+
+UNION
+
+SELECT 'DEPARTMENT_' || DEPARTMENT_ID AS objectid,
+'department' AS set_name,
+DEPARTMENT_ID AS "value",
+DEPARTMENT_DESC AS description,
+DEPARTMENT_NAME AS title,
+DEPARTMENT_ID AS sort_order,
+CASE ACTIVE_YN
+WHEN 'Y' THEN NULL
+WHEN 'N' THEN '1/1/1970'
+END AS date_disabled,
+NULL AS category
+FROM DEPARTMENT
+
+UNION
+
+SELECT 'KEYWORD_' || KEY_WORD_ID AS objectid,
+'keyword' AS set_name,
+KEY_WORD_ID AS "value",
+NULL AS description,
+KEY_WORD_NAME AS title,
+KEY_WORD_ID AS sort_order,
+NULL AS date_disabled,
+NULL AS category
+FROM KEY_WORD
+
+UNION
+
+SELECT 'PROTOCOL_TYPE_' || PROTOCOL_TYPE_ID AS objectid,
+'protocol_type' AS set_name,
+PROTOCOL_TYPE_ID AS "value",
+NAME AS description,
+NAME AS title,
+PROTOCOL_TYPE_ID AS sort_order,
+NULL AS date_disabled,
+NULL AS category
+FROM PROTOCOL_TYPE
+
+UNION
+
+SELECT 'PROTOCOL_CATEGORY_' || ID AS objectid,
+'protocol_category' AS set_name,
+ID AS "value",
+DESCRIPTION AS description,
+NAME AS title,
+ID AS sort_order,
+CASE ACTIVE_YN
+WHEN 'Y' THEN NULL
+WHEN 'N' THEN '1/1/1970'
+END AS date_disabled,
+NULL AS category,
+FROM PROTOCOL_CATEGORY
--- nirc_ehr/resources/queries/study/q_lookup_sets.sql
+++ nirc_ehr/resources/queries/study/q_lookup_sets.sql
@@ -11,3 +11,35 @@ SELECT 'termination_reason_codes' AS setname,
        'value' AS keyField,
        'title'AS titleColumn,
        CAST('2021-07-09' AS TIMESTAMP) AS updated_at
+
+UNION
+
+SELECT 'department' AS setname,
+       'Department' AS label,
+       'value' AS keyField,
+       'title'AS titleColumn,
+       CAST('2021-07-30' AS TIMESTAMP) AS updated_at
+
+UNION
+
+SELECT 'keyword' AS setname,
+       'Keywords' AS label,
+       'value' AS keyField,
+       'title'AS titleColumn,
+       CAST('2021-07-30' AS TIMESTAMP) AS updated_at
+
+UNION
+
+SELECT 'protocol_type' AS setname,
+       'Protocol Type' AS label,
+       'value' AS keyField,
+       'title'AS titleColumn,
+       CAST('2021-07-30' AS TIMESTAMP) AS updated_at
+
+UNION
+
+SELECT 'protocol_category' AS setname,
+       'Protocol Category' AS label,
+       'value' AS keyField,
+       'title'AS titleColumn,
+       CAST('2021-07-30' AS TIMESTAMP) AS updated_at
