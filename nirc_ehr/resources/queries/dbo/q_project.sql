d6cdbfcaeaa03712c2e89763a2c3cad8c8fd8395
labkey-ankurj <ankurj@labkey.com>
2021-08-04 20:26:18 -0700

    correct projects data and project transfer

--- nirc_ehr/resources/queries/dbo/q_project.sql
+++ nirc_ehr/resources/queries/dbo/q_project.sql
@@ -1,9 +1,17 @@
-SELECT ID AS "objectId",
-       ID AS "project",
-       CODE_NUMBER AS "name",
-       DESCRIPTION AS "title",
-       CASE WHEN PROJECT_CATEGORY_ID = 1 THEN 'Standard'
-            WHEN PROJECT_CATEGORY_ID = 20 THEN 'Overhead'
-           END AS "projectCategory",
-       ACTIVE_YN AS "isActive"
-FROM PROJECT_CODE p
+
+SELECT  project AS "name",
+        project AS "title",
+        MAX(ALTERNATE_ID) AS "objectId",
+        MAX(ALTERNATE_ID) AS "project"
+FROM
+(
+    SELECT CASE
+               WHEN substring(alt.NAME, 3, 1) = '-' THEN substring(alt.NAME, 1, 11)
+               WHEN substring(alt.NAME, 5, 1) != '-' THEN substring(alt.NAME, 1, 8)
+               WHEN substring(alt.NAME, 5, 1) = '-' THEN substring(alt.NAME, 1, 11)
+               END AS project,
+           alt.ALTERNATE_ID
+    FROM ALTERNATE alt
+    WHERE alt.ALTERNATE_TYPE_ID = 6 -- Project Inventory Number
+) x
+GROUP BY project
