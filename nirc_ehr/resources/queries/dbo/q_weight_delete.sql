9bc2978df9a70a45eb923b32f4e976cd086c6543
Ankur Juneja <ankurj@labkey.com>
2021-08-24 15:15:09 -0700

    adding incremental strategy to weight ETL (#26)

---
+++ nirc_ehr/resources/queries/dbo/q_weight_delete.sql
@@ -0,0 +1,6 @@
+SELECT substring(adt.PRIMARY_KEY_VALUES, 18) AS objectid,
+       CAST(adt.CHANGE_DATETIME AS TIMESTAMP) AS modified
+FROM AUDIT_TRAIL adt
+WHERE adt.PRIMARY_KEY_VALUES LIKE '%ANIMAL_EVENT_ID%' AND
+        adt.REFERENCE LIKE '%Event: Body Weight%' AND
+        adt.COLUMN_NAME = 'DELETE'
--- nirc_ehr/src/org/labkey/nirc_ehr/NIRC_EHRModule.java
+++ nirc_ehr/src/org/labkey/nirc_ehr/NIRC_EHRModule.java
@@ -45,7 +45,7 @@ public class NIRC_EHRModule extends ExtendedSimpleModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 21.006;
+        return 21.007;
     }
 
     @Override
