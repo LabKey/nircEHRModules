e3a5098f98e998cf18291e0dd29df6b1f0cc23fe
labkey-ankurj <ankurj@labkey.com>
2021-08-24 12:45:16 -0700

    adding incremental strategy to weight ETL

--- nirc_ehr/resources/queries/dbo/q_weight.sql
+++ nirc_ehr/resources/queries/dbo/q_weight.sql
@@ -1,7 +1,13 @@
 
-SELECT anm.ANIMAL_ID_NUMBER AS Id,
+SELECT anmEvt.ANIMAL_EVENT_ID as objectid,
+       anm.ANIMAL_ID_NUMBER AS Id,
        CAST(anmEvt.EVENT_DATETIME AS TIMESTAMP) AS weightDate,
+       CAST(COALESCE (adt.CHANGE_DATETIME,anmEvt.CREATED_DATETIME) AS TIMESTAMP) AS modified,
        anmEvt.result AS weight
 FROM ANIMAL_EVENT anmEvt
 LEFT JOIN ANIMAL anm ON anmEvt.ANIMAL_ID = anm.ANIMAL_ID
+LEFT JOIN AUDIT_TRAIL adt ON anmEvt.ANIMAL_EVENT_ID = substring(adt.PRIMARY_KEY_VALUES, length('ANIMAL_EVENT_ID = '))
+    AND adt.PRIMARY_KEY_VALUES LIKE '%ANIMAL_EVENT_ID%'
+    AND adt.COLUMN_NAME != 'DELETE'
 WHERE anmEvt.EVENT_ID = 1349 -- Body weight
+  AND anmEvt.CREATED_DATETIME < now() -- there are rows in ANIMAL_EVENT table with future dates
---
+++ nirc_ehr/resources/schemas/dbscripts/postgresql/nirc_ehr-21.006-21.007.sql
@@ -0,0 +1,2 @@
+SELECT core.executeJavaUpgradeCode('reloadStudy');
+SELECT core.executeJavaUpgradeCode('etl;{NIRC_EHR}/weight;truncate');
