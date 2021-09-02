2a8351fb6e028ee9628077ba19f09140449d660e
ankurjuneja <ankurjuneja05@gmail.com>
2021-09-01 18:57:46 -0700

    verifying git user

--- nirc_ehr/resources/queries/dbo/q_observations.sql
+++ nirc_ehr/resources/queries/dbo/q_observations.sql
@@ -11,4 +11,4 @@ FROM ANIMAL_EVENT anmEvt
          LEFT JOIN AUDIT_TRAIL adt ON anmEvt.ANIMAL_EVENT_ID = substring(adt.PRIMARY_KEY_VALUES, 18) -- ANIMAL_EVENT_ID =xxxxxx
     AND adt.PRIMARY_KEY_VALUES LIKE '%ANIMAL_EVENT_ID%'
 WHERE anmEvt.EVENT_ID = 1656 --Normal observations
-  AND anmEvt.CREATED_DATETIME < now() -- there are rows in ANIMAL_EVENT table with future dates
+AND anmEvt.CREATED_DATETIME < now() -- there are rows in ANIMAL_EVENT table with future dates
