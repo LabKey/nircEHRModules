1c8f630ae742ded7ce56a9b280b04f159498a12c
ankurjuneja <ankurjuneja05@gmail.com>
2021-09-02 18:44:55 -0700

    obs etl + reporting + inc etl

---
+++ nirc_ehr/resources/queries/dbo/q_observations_delete.sql
@@ -0,0 +1,17 @@
+SELECT substring(adt.PRIMARY_KEY_VALUES, length('ANIMAL_EVENT_ID = ')) AS objectid,
+       CAST(adt.CHANGE_DATETIME AS TIMESTAMP) AS modified,
+       adt.REFERENCE
+FROM AUDIT_TRAIL adt
+WHERE adt.PRIMARY_KEY_VALUES LIKE '%ANIMAL_EVENT_ID%' AND
+        (adt.REFERENCE LIKE '%Normal observations%' OR
+         adt.REFERENCE LIKE '%Trauma%' OR
+         adt.REFERENCE LIKE '%Reduced appetite%' OR
+         adt.REFERENCE LIKE '%Abnormal stool consistency%' OR
+         adt.REFERENCE LIKE '%Dehydration%' OR
+         adt.REFERENCE LIKE '%Decreased activity%' OR
+         adt.REFERENCE LIKE '%Abnormal body condition%' OR
+         adt.REFERENCE LIKE '%Abnormal attitude%' OR
+         adt.REFERENCE LIKE '%No appetite%' OR
+         adt.REFERENCE LIKE '%No Stool%' OR
+         adt.REFERENCE LIKE '%Cage Appendage Entrapment%' ) AND
+        adt.COLUMN_NAME = 'DELETE'
