3308982ef60f737fb8cb9494da58dc781db451f4
ankurjuneja <ankurjuneja05@gmail.com>
2021-09-09 17:05:44 -0700

    vitals ETL and reporting

--- nirc_ehr/src/org/labkey/nirc_ehr/NIRC_EHRModule.java
+++ nirc_ehr/src/org/labkey/nirc_ehr/NIRC_EHRModule.java
@@ -45,7 +45,7 @@ public class NIRC_EHRModule extends ExtendedSimpleModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 21.010;
+        return 21.011;
     }
 
     @Override
