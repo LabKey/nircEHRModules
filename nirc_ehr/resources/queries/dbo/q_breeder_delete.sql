591a38ad6fdb3cbae5424d52fa39e7625a813e4e
labkey-martyp <martyp@labkey.com>
2021-10-05 17:24:24 -0700

    Breeder ETL - source, destination, etl and report

---
+++ nirc_ehr/resources/queries/dbo/q_breeder_delete.sql
@@ -0,0 +1,9 @@
+SELECT substring(adt.PRIMARY_KEY_VALUES, length('ANIMAL_EVENT_ID = ')) AS objectid,
+       CAST(adt.CHANGE_DATETIME AS TIMESTAMP)                            AS modified,
+       adt.REFERENCE
+FROM AUDIT_TRAIL adt
+WHERE adt.PRIMARY_KEY_VALUES LIKE '%ANIMAL_EVENT_ID%' AND
+    (adt.REFERENCE LIKE '%Multiple Sires%' OR
+     adt.REFERENCE LIKE '%Multiple Dams%' OR
+     adt.REFERENCE LIKE '%Mating Event%') AND
+        adt.COLUMN_NAME = 'DELETE'
