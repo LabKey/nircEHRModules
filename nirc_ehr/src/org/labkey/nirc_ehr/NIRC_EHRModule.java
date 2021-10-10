246d87348bc571e822e44d314ecb4f681fcf8006
Binal Patel <binalp@labkey.com>
2021-10-10 14:28:51 -0700

    Update script to include 'Cases' dataset

--- nirc_ehr/src/org/labkey/nirc_ehr/NIRC_EHRModule.java
+++ nirc_ehr/src/org/labkey/nirc_ehr/NIRC_EHRModule.java
@@ -46,7 +46,7 @@ public class NIRC_EHRModule extends ExtendedSimpleModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 21.017;
+        return 21.018;
     }
 
     @Override
