067b49b34579909fd57a9ecf3937a138ca0952b1
Marty Pradere <martyp@labkey.com>
2021-10-15 08:33:09 -0700

    Pregnancy Outcome and Alopecia ETLs and Reports (#53)
    * Pregnancy Outcome and Alopecia incremental ETL with source and delete queries
    * Add animal history reports and update some column titles
    * Clinical Remarks and Breeding group by needed due to audit join duplicates

--- nirc_ehr/resources/queries/dbo/q_alopecia.sql
+++ nirc_ehr/resources/queries/dbo/q_alopecia.sql
@@ -5,11 +5,10 @@ SELECT anmEvt.ANIMAL_EVENT_ID                                                   
             WHEN anmEvt.STAFF_ID.EMAIL_ADDRESS IS NULL THEN 'unknown'
             ELSE substr(anmEvt.STAFF_ID.EMAIL_ADDRESS, 1,
                         instr(anmEvt.STAFF_ID.EMAIL_ADDRESS, '@') - 1) END)      AS performedby,
-       anmEvt.EVENT_ID.NAME                                                      AS type,
-       anmEvt.TEXT_RESULT                                                        AS result,
-       anmEvt.ATTACHMENT_PATH                                                    AS attachmentFile,
+       anmEvt.RESULT                                                             AS score,
+       anmEvt.DIAGNOSIS                                                          AS diagnosis,
        anmCmt.TEXT                                                               AS remark,
-       CAST(COALESCE(adt.CHANGE_DATETIME, anmEvt.CREATED_DATETIME) AS TIMESTAMP) AS modified
+       CAST(COALESCE(max(adt.CHANGE_DATETIME), anmEvt.CREATED_DATETIME) AS TIMESTAMP) AS modified
 FROM ANIMAL_EVENT anmEvt
          LEFT JOIN ANIMAL_EVENT_COMMENT anmCmt ON anmEvt.ANIMAL_EVENT_ID = anmCmt.ANIMAL_EVENT_ID
          LEFT JOIN EVENT_EVENT_GROUP evtEvtGrp ON evtEvtGrp.EVENT_ID = anmEvt.EVENT_ID
@@ -17,8 +16,9 @@ FROM ANIMAL_EVENT anmEvt
                    ON anmEvt.ANIMAL_EVENT_ID = substring(adt.PRIMARY_KEY_VALUES, length('ANIMAL_EVENT_ID = '))
                        AND adt.PRIMARY_KEY_VALUES LIKE '%ANIMAL_EVENT_ID%'
 
-WHERE evtEvtGrp.EVENT_GROUP_ID = 56
---     56 Breeder
+WHERE evtEvtGrp.EVENT_ID IN (2293)
+--     2293	Alopecia
+  AND anmEvt.CREATED_DATETIME < now() -- there are rows in ANIMAL_EVENT table with future dates
 
 -- Joining with event_event_group & audit_trail generates duplicate rows, hence the 'group by'
 GROUP BY anmEvt.ANIMAL_EVENT_ID,
@@ -26,8 +26,8 @@ GROUP BY anmEvt.ANIMAL_EVENT_ID,
          anmEvt.EVENT_DATETIME,
          anmEvt.STAFF_ID.EMAIL_ADDRESS,
          anmEvt.EVENT_ID.NAME,
-         anmEvt.TEXT_RESULT,
-         anmEvt.ATTACHMENT_PATH,
+         anmEvt.RESULT,
+         anmEvt.DIAGNOSIS,
          anmCmt.TEXT,
          anmEvt.CREATED_DATETIME,
          adt.CHANGE_DATETIME
