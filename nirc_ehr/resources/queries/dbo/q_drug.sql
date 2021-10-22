dda95f3e91a882dde77e233de3afe964aa022664
labkey-martyp <martyp@labkey.com>
2021-10-22 05:54:02 -0700

    Group by still needed on q_drug.sql

--- nirc_ehr/resources/queries/dbo/q_drug.sql
+++ nirc_ehr/resources/queries/dbo/q_drug.sql
@@ -23,3 +23,14 @@ WHERE (evtEvtGrp.EVENT_GROUP_ID = 31 AND evtEvtGrp.EVENT_ID = 2261) -- Sedation 
 --     51	Oral Dose Administration - Alert
 --     54	Fluid Administration
 --     65	Sedation
+
+GROUP BY anmEvt.ANIMAL_EVENT_ID,
+         anmEvt.ANIMAL_ID.ANIMAL_ID_NUMBER,
+         anmEvt.EVENT_DATETIME,
+         staff.email_prefix,
+         anmEvt.EVENT_ID.NAME,
+         anmEvt.RESULT,
+         anmEvt.ATTACHMENT_PATH,
+         anmCmt.TEXT,
+         anmEvt.CREATED_DATETIME,
+         adt.modified
