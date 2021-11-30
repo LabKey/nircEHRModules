03ffd2c3ca5a342f363ac9a8991f5ea830e995cf
Ankur Juneja <ankurjuneja05@gmail.com>
2021-11-29 19:04:24 -0800

    Fix projects and protocols ETL (#65)

--- nirc_ehr/src/org/labkey/nirc_ehr/NIRC_EHRModule.java
+++ nirc_ehr/src/org/labkey/nirc_ehr/NIRC_EHRModule.java
@@ -48,7 +48,7 @@ public class NIRC_EHRModule extends ExtendedSimpleModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 21.020;
+        return 21.021;
     }
 
     @Override
