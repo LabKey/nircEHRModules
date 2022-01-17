097d58e5ba9953f45d0c732a3b46999d5ade5fc2
labkey-martyp <martyp@labkey.com>
2022-01-17 11:46:37 -0800

    Remove unnecessary concat

--- nirc_ehr/resources/queries/dbo/q_arrival.sql
+++ nirc_ehr/resources/queries/dbo/q_arrival.sql
@@ -1,4 +1,4 @@
-SELECT '' || anmEvt.ANIMAL_EVENT_ID   AS objectid,
+SELECT anmEvt.ANIMAL_EVENT_ID   AS objectid,
        anm.ANIMAL_ID_NUMBER  AS Id,
        (CASE
             WHEN (anmEvt.STAFF_ID.STAFF_FIRST_NAME IS NULL OR anmEvt.STAFF_ID.STAFF_LAST_NAME IS NULL) THEN 'unknown'
