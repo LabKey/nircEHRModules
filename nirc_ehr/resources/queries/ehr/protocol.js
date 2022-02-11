d1ca072c8be1c28d9ef8aff6f9cf859b1b20da65
labkey-martyp <martyp@labkey.com>
2022-02-11 04:50:18 -0800

    CR feedback

--- nirc_ehr/resources/queries/ehr/protocol.js
+++ nirc_ehr/resources/queries/ehr/protocol.js
@@ -6,30 +6,27 @@ EHR.Server.TriggerManager.registerHandlerForQuery(EHR.Server.TriggerManager.Even
     if (row.description) {
         plainTextParts = row.description.split("\\loch\\af0")
         let plainText = "";
-        for(let i = 0; i < plainTextParts.length; i++)
+        for(let i = 1; i < plainTextParts.length; i++)
         {
-            if (i > 0)
-            {
-                let chunk = LABKEY.Utils.encodeHtml(plainTextParts[i].split("}")[0]);
-                chunk = chunk.replace("\\hich\\af0 \\&#39;85", "&#8230;"); // ellipsis
-                chunk = chunk.replace("\\hich\\af0 \\&#39;92", "'");
-                chunk = chunk.replace("\\hich\\af0 \\&#39;93", "\"");
-                chunk = chunk.replace("\\hich\\af0 \\&#39;94", "\"");
-                chunk = chunk.replace("\\hich\\af0 \\&#39;99", "&#8482;"); // trademark
-                chunk = chunk.replace("\\hich\\af0 \\&#39;ae", "&#174;");  // registered trademark
-                chunk = chunk.replace("\\hich\\af0 \\&#39;b0", "&#176;"); // Degree sign
-                chunk = chunk.replace("\\hich\\af0 \\&#39;b5", "&#181;"); // Micro sign (Greek mu)
-                chunk = chunk.replace("\\hich\\af0 \\&#39;b7", "&#183;"); // Middle dot
-                chunk = chunk.replace("\\hich\\af0 \\&#39;ef", "&#239;"); // i-diaeresis
-                // TODO: As a general catch all, could convert the hex value in the match to the HTML ascii entity
-                // Currently this covers all the cases though in the source data
+            let chunk = LABKEY.Utils.encodeHtml(plainTextParts[i].split("}")[0]);
+            chunk = chunk.replace("\\hich\\af0 \\&#39;85", "&#8230;"); // ellipsis
+            chunk = chunk.replace("\\hich\\af0 \\&#39;92", "'");
+            chunk = chunk.replace("\\hich\\af0 \\&#39;93", "\"");
+            chunk = chunk.replace("\\hich\\af0 \\&#39;94", "\"");
+            chunk = chunk.replace("\\hich\\af0 \\&#39;99", "&#8482;"); // trademark
+            chunk = chunk.replace("\\hich\\af0 \\&#39;ae", "&#174;");  // registered trademark
+            chunk = chunk.replace("\\hich\\af0 \\&#39;b0", "&#176;"); // Degree sign
+            chunk = chunk.replace("\\hich\\af0 \\&#39;b5", "&#181;"); // Micro sign (Greek mu)
+            chunk = chunk.replace("\\hich\\af0 \\&#39;b7", "&#183;"); // Middle dot
+            chunk = chunk.replace("\\hich\\af0 \\&#39;ef", "&#239;"); // i-diaeresis
+            // TODO: As a general catch all, could convert the hex value in the match to the HTML ascii entity
+            // Currently this covers all the cases though in the source data
 
-                plainText += chunk.trim();
-                plainText += " ";
-            }
+            plainText += chunk.trim();
+            plainText += " ";
+
         }
         row.description = plainText.trim();
-        // console.log(row.description);
     }
 
 });
--- nirc_ehr/resources/queries/nirc_ehr/QuestionResponse.js
+++ nirc_ehr/resources/queries/nirc_ehr/QuestionResponse.js
@@ -2,34 +2,29 @@ require("ehr/triggers").initScript(this);
 
 EHR.Server.TriggerManager.registerHandlerForQuery(EHR.Server.TriggerManager.Events.BEFORE_INSERT, 'nirc_ehr', 'QuestionResponse', function (helper, scriptErrors, row, oldRow) {
 
-    // console.log("nirc_ehr.QuestionResponse trigger")
     if (row.Response) {
         plainTextParts = row.Response.split("\\loch\\af0")
         let plainText = "";
-        for(let i = 0; i < plainTextParts.length; i++)
+        for(let i = 1; i < plainTextParts.length; i++)
         {
-            if (i > 0)
-            {
-                let chunk = LABKEY.Utils.encodeHtml(plainTextParts[i].split("}")[0]);
-                chunk = chunk.replace("\\hich\\af0 \\&#39;85", "&#8230;"); // ellipsis
-                chunk = chunk.replace("\\hich\\af0 \\&#39;92", "'");
-                chunk = chunk.replace("\\hich\\af0 \\&#39;93", "\"");
-                chunk = chunk.replace("\\hich\\af0 \\&#39;94", "\"");
-                chunk = chunk.replace("\\hich\\af0 \\&#39;99", "&#8482;"); // trademark
-                chunk = chunk.replace("\\hich\\af0 \\&#39;ae", "&#174;");  // registered trademark
-                chunk = chunk.replace("\\hich\\af0 \\&#39;b0", "&#176;"); // Degree sign
-                chunk = chunk.replace("\\hich\\af0 \\&#39;b5", "&#181;"); // Micro sign (Greek mu)
-                chunk = chunk.replace("\\hich\\af0 \\&#39;b7", "&#183;"); // Middle dot
-                chunk = chunk.replace("\\hich\\af0 \\&#39;ef", "&#239;"); // i-diaeresis
-                // TODO: As a general catch all, could convert the hex value in the match to the HTML ascii entity
-                // Currently this covers all the cases though in the source data
+            let chunk = LABKEY.Utils.encodeHtml(plainTextParts[i].split("}")[0]);
+            chunk = chunk.replace("\\hich\\af0 \\&#39;85", "&#8230;"); // ellipsis
+            chunk = chunk.replace("\\hich\\af0 \\&#39;92", "'");
+            chunk = chunk.replace("\\hich\\af0 \\&#39;93", "\"");
+            chunk = chunk.replace("\\hich\\af0 \\&#39;94", "\"");
+            chunk = chunk.replace("\\hich\\af0 \\&#39;99", "&#8482;"); // trademark
+            chunk = chunk.replace("\\hich\\af0 \\&#39;ae", "&#174;");  // registered trademark
+            chunk = chunk.replace("\\hich\\af0 \\&#39;b0", "&#176;"); // Degree sign
+            chunk = chunk.replace("\\hich\\af0 \\&#39;b5", "&#181;"); // Micro sign (Greek mu)
+            chunk = chunk.replace("\\hich\\af0 \\&#39;b7", "&#183;"); // Middle dot
+            chunk = chunk.replace("\\hich\\af0 \\&#39;ef", "&#239;"); // i-diaeresis
+            // TODO: As a general catch all, could convert the hex value in the match to the HTML ascii entity
+            // Currently this covers all the cases though in the source data
 
-                plainText += chunk.trim();
-                plainText += " ";
-            }
+            plainText += chunk.trim();
+            plainText += " ";
         }
         row.Response = plainText.trim();
-        // console.log(row.Response);
     }
 
 });
