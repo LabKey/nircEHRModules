f420e14d2fb59cba31369f9297ae5771cfe01826
labkey-martyp <martyp@labkey.com>
2022-03-18 09:49:46 -0700

    Reload study

--- nirc_ehr/src/org/labkey/nirc_ehr/NIRC_EHRModule.java
+++ nirc_ehr/src/org/labkey/nirc_ehr/NIRC_EHRModule.java
@@ -58,7 +58,7 @@ public class NIRC_EHRModule extends ExtendedSimpleModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 22.009;
+        return 22.010;
     }
 
     @Override
