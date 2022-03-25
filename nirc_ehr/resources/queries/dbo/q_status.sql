ace416ec9b2316127242fd4758619508af1d873c
labkey-martyp <martyp@labkey.com>
2022-03-25 12:29:28 -0700

    Add invalid status

--- nirc_ehr/resources/queries/dbo/q_status.sql
+++ nirc_ehr/resources/queries/dbo/q_status.sql
@@ -1,8 +1,9 @@
 -- Used in q_demographics
 SELECT anm.ANIMAL_ID_NUMBER AS Id,
        anm.ANIMAL_DISPOSITION_ID.NAME,
-       CASE WHEN anm.ANIMAL_DISPOSITION_ID IS NULL THEN 'Alive'
-       WHEN anm.ANIMAL_DISPOSITION_ID IN (2, 3, 4) THEN 'Dead'  -- Died, Project Sacrifice, Moribund Sacrifice
-       WHEN anm.ANIMAL_DISPOSITION_ID IN (5, 6) THEN 'Other'   -- Escaped, Misc
-       ELSE 'Shipped' END AS status
+       CASE WHEN anm.TERMINATION_REASON_ID.ID = 10 THEN 'Invalid'   -- Invalid Id
+            WHEN anm.ANIMAL_DISPOSITION_ID IS NULL THEN 'Alive'
+            WHEN anm.ANIMAL_DISPOSITION_ID IN (2, 3, 4) THEN 'Dead'  -- Died, Project Sacrifice, Moribund Sacrifice
+            WHEN anm.ANIMAL_DISPOSITION_ID IN (5, 6) THEN 'Other'   -- Escaped, Misc
+            ELSE 'Shipped' END AS status
 FROM Animal anm
