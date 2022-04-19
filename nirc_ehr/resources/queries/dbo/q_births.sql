e5a306601c8b8faf91693b7c070ff3245e512acd
labkey-martyp <martyp@labkey.com>
2022-04-18 20:16:27 -0700

    Fix incremental filters

--- nirc_ehr/resources/queries/dbo/q_births.sql
+++ nirc_ehr/resources/queries/dbo/q_births.sql
@@ -5,7 +5,7 @@ SELECT anm.ANIMAL_ID_NUMBER AS Id,
        COALESCE(MAX(CAST(adt.CHANGE_DATETIME AS TIMESTAMP)), to_date('01/01/1970' ,'MM/DD/YYYY')) AS modified
 FROM Animal anm
 LEFT JOIN ALTERNATE alt ON alt.ANIMAL_ID = anm.ANIMAL_ID
-LEFT JOIN AUDIT_TRAIL adt ON anm.ANIMAL_ID_NUMBER = substring(PRIMARY_KEY_VALUES, length('ANIMAL_ID = '))
+LEFT JOIN AUDIT_TRAIL adt ON anm.ANIMAL_ID = substring(PRIMARY_KEY_VALUES, length('ANIMAL_ID = '))
     AND adt.TABLE_NAME = 'ANIMAL'
 WHERE alt.ALTERNATE_TYPE_ID = 7 -- 'Dam/Sire/Acq'
 AND anm.ANIMAL_ID_NUMBER LIKE 'A%' -- Animal born in centers are pre-appended with A's (in animal Id)
