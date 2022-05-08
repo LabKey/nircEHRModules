cf3a316cc089ce8c4994b02c8ae4622cacdd5995
labkey-martyp <martyp@labkey.com>
2022-05-08 06:52:33 -0700

    ETL updates

--- nirc_ehr/resources/queries/dbo/q_births.sql
+++ nirc_ehr/resources/queries/dbo/q_births.sql
@@ -2,15 +2,18 @@ SELECT anm.ANIMAL_ID_NUMBER AS Id,
        anm.BIRTH_DATE       AS birthDate,
        anm.GENDER_ID        AS gender,
        anm.SSB_ID.SPECIES_ID           AS species,
-       COALESCE(MAX(CAST(adt.CHANGE_DATETIME AS TIMESTAMP)), to_date('01/01/1970' ,'MM/DD/YYYY')) AS modified
+       -- audit timestamp for modifications or animal event received for created
+       COALESCE(MAX(CAST(adt.CHANGE_DATETIME AS TIMESTAMP)), ae.CREATED_DATETIME) AS modified
 FROM Animal anm
 LEFT JOIN ALTERNATE alt ON alt.ANIMAL_ID = anm.ANIMAL_ID
 LEFT JOIN AUDIT_TRAIL adt ON anm.ANIMAL_ID = substring(PRIMARY_KEY_VALUES, length('ANIMAL_ID = '))
     AND adt.TABLE_NAME = 'ANIMAL'
+LEFT JOIN ANIMAL_EVENT ae ON ae.ANIMAL_ID = anm.ANIMAL_ID AND EVENT_ID = 1  -- Received
 WHERE alt.ALTERNATE_TYPE_ID = 7 -- 'Dam/Sire/Acq'
 AND anm.ANIMAL_ID_NUMBER LIKE 'A%' -- Animal born in centers are pre-appended with A's (in animal Id)
 AND alt.DESCRIPTION IS NOT NULL
 GROUP BY anm.ANIMAL_ID_NUMBER,
          anm.BIRTH_DATE,
          anm.GENDER_ID,
-         anm.SSB_ID.SPECIES_ID
+         anm.SSB_ID.SPECIES_ID,
+         ae.CREATED_DATETIME
