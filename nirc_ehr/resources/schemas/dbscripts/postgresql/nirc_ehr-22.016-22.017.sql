87b9e1c8f8ebbfcc2e7d746819a2748fa25b5fd2
Marty Pradere <martyp@labkey.com>
2022-05-16 18:24:42 -0700

    Update projects and protocol assignments (#143)
    - Pull projects from Alternate table
    - Updates to protocol assignment trigger script

---
+++ nirc_ehr/resources/schemas/dbscripts/postgresql/nirc_ehr-22.016-22.017.sql
@@ -0,0 +1,4 @@
+SELECT core.executeJavaUpgradeCode('etl;{NIRC_EHR}/project;truncate');
+SELECT core.executeJavaUpgradeCode('etl;{NIRC_EHR}/assignment;truncate');
+SELECT core.executeJavaUpgradeCode('etl;{NIRC_EHR}/protocol;truncate');
+SELECT core.executeJavaUpgradeCode('etl;{NIRC_EHR}/protocolAssignment;truncate');
