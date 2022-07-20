6b9c14382b4922c79f90296edce24957a176cb3c
labkey-martyp <martyp@labkey.com>
2022-07-20 16:56:09 -0700

    Simplify project and protocol ETLs. Cleanup deprecated warnings

--- nirc_ehr/resources/queries/dbo/q_protocol_delete.sql
+++
@@ -1,6 +0,0 @@
-SELECT substring(adt.PRIMARY_KEY_VALUES, length('PROTOCOL_ID = ')) AS protocol,
-       CAST(adt.CHANGE_DATETIME AS TIMESTAMP)                      AS modified,
-       adt.REFERENCE
-FROM AUDIT_TRAIL adt
-WHERE adt.TABLE_NAME = 'PROTOCOL'
-       AND adt.COLUMN_NAME = 'DELETE'
