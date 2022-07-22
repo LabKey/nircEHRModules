d8157c3121d1491069570b02b7ebb2a330b2d1aa
Marty Pradere <martyp@labkey.com>
2022-07-22 11:35:13 -0700

    Simplify project and protocol ETLs. Cleanup deprecated warnings (#156)
    * Projects and project assignments put in one ETL to ensure dependencies. Converted both to truncate options.
    * Protocol, protocol assignments and protocol ammendments put in one ETL and converted to truncate options.
    * Fix java customizer deprecated warnings.

--- nirc_ehr/src/org/labkey/nirc_ehr/NIRC_EHRModule.java
+++ nirc_ehr/src/org/labkey/nirc_ehr/NIRC_EHRModule.java
@@ -58,7 +58,7 @@ public class NIRC_EHRModule extends ExtendedSimpleModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 22.018;
+        return 22.019;
     }
 
     @Override
