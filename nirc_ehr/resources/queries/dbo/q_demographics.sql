634f4733e7bb0b9820c652eaae0cd9a6e08ba3a1
NIRC-IT <87669815+NIRC-IT@users.noreply.github.com>
2023-03-10 09:00:16 -0600

    NIRC ETL and History report changes

--- nirc_ehr/resources/queries/dbo/q_demographics.sql
+++ nirc_ehr/resources/queries/dbo/q_demographics.sql
@@ -9,12 +9,12 @@ SELECT anm.ANIMAL_ID_NUMBER AS participantId,
        anm.ANIMAL_DISPOSITION_ID AS disposition,
        anm.DESCRIPTION AS description,
        anm.TERMINATION_REASON_ID AS termination,
-       anm.IMPLANT_NUMBER AS implantNumber,
        anm.VENDOR_ANIMAL_NUMBER AS vendorNumber,
        anm.ACTIVE_YN AS activeStatus,
        anm.LOT_NUMBER_ID AS lotId,
        st.status AS calculated_status,
        alt.NAME AS damSire,
+       altImplant.Name AS implantNumber,
        altOrigin.Name AS geographic_origin,
        altSrc.Name AS source,
        altCites.Name AS CITES,
@@ -26,6 +26,7 @@ AND adt.TABLE_NAME = 'ANIMAL'
 LEFT JOIN q_status st ON st.Id = anm.ANIMAL_ID_NUMBER
 LEFT JOIN ANIMAL_EVENT ae ON ae.ANIMAL_ID = anm.ANIMAL_ID AND EVENT_ID = 1  -- Received
 LEFT JOIN ALTERNATE alt ON alt.ANIMAL_ID = anm.ANIMAL_ID AND alt.ALTERNATE_TYPE_ID = 7  -- Dam/sire
+LEFT JOIN ALTERNATE altImplant ON altImplant.ANIMAL_ID = anm.ANIMAL_ID AND altImplant.ALTERNATE_TYPE_ID = 5 --Transponder number
 LEFT JOIN ALTERNATE altOrigin ON altOrigin.ANIMAL_ID = anm.ANIMAL_ID AND altOrigin.ALTERNATE_TYPE_ID = 10  -- Country of origin
 LEFT JOIN ALTERNATE altSrc ON altSrc.ANIMAL_ID = anm.ANIMAL_ID AND altSrc.ALTERNATE_TYPE_ID = 11  -- Source
 LEFT JOIN ALTERNATE altCites ON altCites.ANIMAL_ID = anm.ANIMAL_ID AND altCites.ALTERNATE_TYPE_ID = 3  -- CITES number
@@ -40,12 +41,12 @@ GROUP BY anm.ANIMAL_ID_NUMBER,
     anm.ANIMAL_DISPOSITION_ID,
     anm.DESCRIPTION,
     anm.TERMINATION_REASON_ID,
-    anm.IMPLANT_NUMBER,
     anm.VENDOR_ANIMAL_NUMBER,
     anm.ACTIVE_YN,
     anm.LOT_NUMBER_ID,
     st.status,
     alt.NAME,
+    altImplant.Name,
     altOrigin.Name,
     altSrc.Name,
     altCites.Name,
