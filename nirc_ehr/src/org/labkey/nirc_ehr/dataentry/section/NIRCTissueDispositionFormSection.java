5346481e66b6ac2dd3164387ec911cf73001d5ba
Binal Patel <binalp@labkey.com>
2024-02-06 15:42:52 -0800

    Tissue Disposition section updates

--- nirc_ehr/src/org/labkey/nirc_ehr/dataentry/section/NIRCTissueDispositionFormSection.java
+++ nirc_ehr/src/org/labkey/nirc_ehr/dataentry/section/NIRCTissueDispositionFormSection.java
@@ -1,9 +1,30 @@
 package org.labkey.nirc_ehr.dataentry.section;
 
+import java.util.List;
+
 public class NIRCTissueDispositionFormSection extends BaseFormSection
 {
     public NIRCTissueDispositionFormSection()
     {
         super("study", "tissueDisposition", "Tissue Disposition", "ehr-gridpanel", true, true);
+    }
+
+    @Override
+    public List<String> getTbarButtons()
+    {
+        List<String> defaults = super.getTbarButtons();
+        defaults.remove("COPYFROMSECTION");
+        defaults.remove("ADDANIMALS");
+        defaults.remove("TEMPLATE");
+        return defaults;
+    }
+
+    @Override
+    public List<String> getTbarMoreActionButtons()
+    {
+        List<String> defaultButtons = super.getTbarMoreActionButtons();
+        defaultButtons.remove("GUESSPROJECT");
+        defaultButtons.remove("COPY_IDS");
+        return defaultButtons;
     }
 }
