71ace5f73a4042901739586d088e3ac919ba7536
Binal Patel <binalp@labkey.com>
2024-02-21 14:29:10 -0800

    Add editable_lookups, test update.

--- nirc_ehr/test/src/org.labkey.test/tests.nirc_ehr/NIRC_EHRTest.java
+++ nirc_ehr/test/src/org.labkey.test/tests.nirc_ehr/NIRC_EHRTest.java
@@ -19,23 +19,32 @@ package org.labkey.test.tests.nirc_ehr;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.labkey.api.reader.Readers;
 import org.labkey.test.Locator;
+import org.labkey.test.ModulePropertyValue;
 import org.labkey.test.TestFileUtils;
 import org.labkey.test.WebTestHelper;
 import org.labkey.test.categories.EHR;
+import org.labkey.test.components.ui.grids.QueryGrid;
+import org.labkey.test.pages.jhu_ehr.EHRLookupPage;
 import org.labkey.test.tests.ehr.AbstractGenericEHRTest;
 import org.labkey.test.util.DataRegionTable;
 import org.labkey.test.util.Ext4Helper;
+import org.labkey.test.util.LogMethod;
 import org.labkey.test.util.PortalHelper;
 import org.labkey.test.util.PostgresOnlyTest;
 import org.labkey.test.util.ext4cmp.Ext4ComboRef;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+import static org.labkey.test.components.html.Input.Input;
 
 @Category({EHR.class})
 public class NIRC_EHRTest extends AbstractGenericEHRTest implements PostgresOnlyTest
@@ -92,6 +101,32 @@ public class NIRC_EHRTest extends AbstractGenericEHRTest implements PostgresOnly
     protected File getStudyPolicyXML()
     {
         return TestFileUtils.getSampleData("nircEHRStudyPolicy.xml");
+    }
+
+    @Override
+    @LogMethod
+    protected void populateInitialData() throws Exception
+    {
+        List<ModulePropertyValue> props = new ArrayList<>();
+        props.add(new ModulePropertyValue("EHR", "/" + getProjectName(), "EHRCustomModule", "NIRC_EHR"));
+        goToProjectHome();
+        setModuleProperties(props);
+
+        beginAt(WebTestHelper.buildURL("ehr", getContainerPath(), "populateLookupData", Map.of("manifest", "lookupsManifestTest")));
+
+        waitForElement(Locator.linkWithText("Populate Lookups"));
+        click(Locator.linkWithText("Populate Lookups"));
+        acceptAlert();
+
+        waitFor(() -> Input(Locator.textarea("populateLookupResults"), getDriver()).waitFor().getValue().contains("Loading lookups is complete."),
+                "Lookups didn't finish loading", 60000);
+
+        waitForElement(Locator.linkWithText("Populate Reports"));
+        click(Locator.linkWithText("Populate Reports"));
+        acceptAlert();
+
+        waitFor(() -> Input(Locator.textarea("populateLookupResults"), getDriver()).waitFor().getValue().contains("Loading reports is complete."),
+                "Reports didn't finish loading", 60000);
     }
 
     @BeforeClass
@@ -151,5 +186,34 @@ public class NIRC_EHRTest extends AbstractGenericEHRTest implements PostgresOnly
         int columnCount = table.getColumnCount();
         List<String> row = table.getRowDataAsText(0);
         assertEquals("Calculated ages are incorrect", Arrays.asList("4.0", "4.0", "48.0"), row.subList(columnCount - 3, columnCount));
+    }
+
+    @Test
+    public void testLookupPage() throws Exception
+    {
+        goToEHRFolder();
+        clickAndWait(Locator.linkWithText("Manage Lookup Tables"));
+
+        EHRLookupPage ehrLookupPage = new EHRLookupPage(this);
+        ehrLookupPage.waitForPage();
+        QueryGrid grid = ehrLookupPage.getQueryGrid();
+        checker().verifyEquals("Missing look up tables", countLines(TestFileUtils.getLabKeyRoot() + getModulePath() + "/resources/data/editable_lookups.tsv") - 1, grid.getRecordCount());
+
+        clickAndWait(Locator.linkWithText("Age Class"));
+        checker().verifyEquals("Navigated to incorrect schema", "ehr_lookups", getUrlParam("schemaName"));
+        checker().verifyEquals("Navigated to incorrect query", "ageclass", getUrlParam("query.queryName"));
+    }
+
+    private int countLines(String filePath) throws Exception
+    {
+        try (BufferedReader reader = Readers.getReader(new File(filePath)))
+        {
+            int count = 0;
+            while (reader.readLine() != null)
+            {
+                count++;
+            }
+            return count;
+        }
     }
 }
