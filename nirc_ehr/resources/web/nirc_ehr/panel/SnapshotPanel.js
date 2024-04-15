e35515bafadb1bfc06296d1356748050446c4d2d
ankurjuneja <ankurjuneja05@gmail.com>
2024-04-15 15:31:23 -0700

    cr feedback

--- nirc_ehr/resources/web/nirc_ehr/panel/SnapshotPanel.js
+++ nirc_ehr/resources/web/nirc_ehr/panel/SnapshotPanel.js
@@ -2,6 +2,100 @@ Ext4.define('NIRC_EHR.panel.SnapshotPanel', {
     extend: 'EHR.panel.SnapshotPanel',
     alias: 'widget.nirc_ehr-snapshotpanel',
 
+    getBaseItems: function(){
+        return [{
+            xtype: 'container',
+            border: false,
+            defaults: {
+                border: false
+            },
+            items: [{
+                xtype: 'container',
+                html: '<b>Summary:</b><hr>'
+            },{
+                bodyStyle: 'padding-bottom: 20px;',
+                layout: 'column',
+                defaults: {
+                    border: false
+                },
+                items: [{
+                    xtype: 'container',
+                    columnWidth: 0.25,
+                    defaults: {
+                        labelWidth: this.defaultLabelWidth,
+                        style: 'margin-right: 20px;'
+                    },
+                    items: [{
+                        xtype: 'displayfield',
+                        fieldLabel: 'Location',
+                        name: 'location'
+                    },{
+                        xtype: 'displayfield',
+                        hidden: this.redacted,
+                        name: 'assignments',
+                        fieldLabel: 'Assignments'
+                    },{
+                        xtype: 'displayfield',
+                        fieldLabel: 'Source',
+                        name: 'source'
+                    },{
+                        xtype: 'displayfield',
+                        fieldLabel: 'Prev Id',
+                        name: 'prev_id'
+                    }]
+                },{
+                    xtype: 'container',
+                    columnWidth: 0.25,
+                    defaults: {
+                        labelWidth: this.defaultLabelWidth,
+                        style: 'margin-right: 20px;'
+                    },
+                    items: [{
+                        xtype: 'displayfield',
+                        fieldLabel: 'Status',
+                        name: 'calculated_status'
+                    },{
+                        xtype: 'displayfield',
+                        fieldLabel: 'Sex',
+                        name: 'gender'
+                    },{
+                        xtype: 'displayfield',
+                        fieldLabel: 'Species',
+                        name: 'species'
+                    },{
+                        xtype: 'displayfield',
+                        fieldLabel: 'Age',
+                        name: 'age'
+                    }]
+                },{
+                    xtype: 'container',
+                    columnWidth: 0.35,
+                    defaults: {
+                        labelWidth: this.defaultLabelWidth,
+                        style: 'margin-right: 20px;'
+                    },
+                    items: [{
+                        xtype: 'displayfield',
+                        fieldLabel: 'Flags',
+                        name: 'flags'
+                    },{
+                        xtype: 'displayfield',
+                        fieldLabel: 'Remark',
+                        name: 'remark'
+                    },{
+                        xtype: 'displayfield',
+                        fieldLabel: 'Last TB',
+                        name: 'lastTB'
+                    },{
+                        xtype: 'displayfield',
+                        fieldLabel: 'Weights',
+                        name: 'weights'
+                    }]
+                }]
+            }]
+        }];
+    },
+
     appendDemographicsResults: function(toSet, row, id){
         if (!row){
             console.log('Id not found');
@@ -86,12 +180,15 @@ Ext4.define('NIRC_EHR.panel.SnapshotPanel', {
         var values = [];
         if (results){
             Ext4.each(results, function(row){
-                var val = row['protocolTitle'] || '';
+                let val = '';
+                if (row['protocolTitle'])
+                    val += row['protocolTitle'];
                 val += ' - ' + (row['investigatorLastName'] || row['investigatorId'] || row['investigatorName']);
-                val += ' - ' + (row['project']);
+                values.push(LABKEY.Utils.encodeHtml(val));
+                if (row['project']) {
+                    values.push("Project - " + LABKEY.Utils.encodeHtml(row['project']));
+                }
 
-                if (val)
-                    values.push(LABKEY.Utils.encodeHtml(val));
             }, this);
         }
 
