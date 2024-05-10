7b551a2799ec67ea82497faf91220a85a34772e7
Binal Patel <binalp@labkey.com>
2024-05-10 13:43:00 -0700

    fix script

--- nirc_ehr/resources/schemas/dbscripts/postgresql/nirc_ehr-24.004-24.005.sql
+++ nirc_ehr/resources/schemas/dbscripts/postgresql/nirc_ehr-24.004-24.005.sql
@@ -1,4 +1,4 @@
 SELECT core.executeJavaUpgradeCode('reloadFolder');
 
 SELECT core.executeJavaUpgradeCode('etl;{NIRC_EHR}/arrival;truncate');
-SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;lookup_sets;/data/source.tsv');
+SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;source;/data/source.tsv');
