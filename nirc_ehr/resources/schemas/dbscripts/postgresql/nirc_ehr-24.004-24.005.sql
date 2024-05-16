71fadcf4a302770d6d361806bb3a51d2cfdcd2fb
Binal Patel <binalp@labkey.com>
2024-05-15 18:48:38 -0700

    Add geographic_origins

--- nirc_ehr/resources/schemas/dbscripts/postgresql/nirc_ehr-24.004-24.005.sql
+++ nirc_ehr/resources/schemas/dbscripts/postgresql/nirc_ehr-24.004-24.005.sql
@@ -4,3 +4,4 @@ SELECT core.executeJavaUpgradeCode('etl;{NIRC_EHR}/arrival;truncate');
 SELECT core.executeJavaUpgradeCode('etl;{NIRC_EHR}/drug;truncate');
 SELECT core.executeJavaUpgradeCode('etl;{NIRC_EHR}/blood;truncate');
 SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;source;/data/source.tsv');
+SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;geographic_origins;/data/geographic_origins.tsv');
