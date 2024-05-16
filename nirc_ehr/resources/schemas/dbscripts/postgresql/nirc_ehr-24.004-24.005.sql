00de3b72423100b0e70237a114f9a70561f3244a
Binal Patel <binalp@labkey.com>
2024-05-15 18:47:05 -0700

    Add drug and blood

--- nirc_ehr/resources/schemas/dbscripts/postgresql/nirc_ehr-24.004-24.005.sql
+++ nirc_ehr/resources/schemas/dbscripts/postgresql/nirc_ehr-24.004-24.005.sql
@@ -1,4 +1,6 @@
 SELECT core.executeJavaUpgradeCode('reloadFolder');
 
 SELECT core.executeJavaUpgradeCode('etl;{NIRC_EHR}/arrival;truncate');
+SELECT core.executeJavaUpgradeCode('etl;{NIRC_EHR}/drug;truncate');
+SELECT core.executeJavaUpgradeCode('etl;{NIRC_EHR}/blood;truncate');
 SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;source;/data/source.tsv');
