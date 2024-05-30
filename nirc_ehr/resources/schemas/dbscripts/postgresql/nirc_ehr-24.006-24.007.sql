6eb6b1b5692f7db1b93ff199cfc04020f3077e03
ankurjuneja <ankurjuneja05@gmail.com>
2024-05-30 08:03:43 -0700

    new lookups data

--- nirc_ehr/resources/schemas/dbscripts/postgresql/nirc_ehr-24.006-24.007.sql
+++ nirc_ehr/resources/schemas/dbscripts/postgresql/nirc_ehr-24.006-24.007.sql
@@ -1,1 +1,14 @@
 SELECT core.executeJavaUpgradeCode('reloadFolder');
+
+SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;lookup_sets;/data/lookup_sets.tsv');
+SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;app_score;/data/app_score.tsv');
+SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;bcs_score;/data/bcs_score.tsv');
+SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;blood_draw_reason;/data/blood_draw_reason.tsv');
+SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;blood_draw_tube_type;/data/blood_draw_tube_type.tsv');
+SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;blood_sample_type;/data/blood_sample_type.tsv');
+SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;blood_tube_volumes;/data/blood_tube_volumes.tsv');
+SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;hyd_score;/data/hyd_score.tsv');
+SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;observation_types;/data/observation_types.tsv');
+SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;observation_areas;/data/observation_areas.tsv');
+SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;stool_score;/data/stool_score.tsv');
+SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;editable_lookups;/data/editable_lookups.tsv');
