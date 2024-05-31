a8548808ebdd2278730a5d3dc1dbf3f305e0e1b9
ankurjuneja <ankurjuneja05@gmail.com>
2024-05-31 11:40:46 -0700

    correct import of lookups data

--- nirc_ehr/resources/schemas/dbscripts/postgresql/nirc_ehr-24.006-24.007.sql
+++ nirc_ehr/resources/schemas/dbscripts/postgresql/nirc_ehr-24.006-24.007.sql
@@ -8,7 +8,7 @@ SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;blood_draw_tube_ty
 SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;blood_sample_type;/data/blood_sample_type.tsv');
 SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;blood_tube_volumes;/data/blood_tube_volumes.tsv');
 SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;hyd_score;/data/hyd_score.tsv');
-SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;observation_types;/data/observation_types.tsv');
+SELECT core.executeJavaUpgradeCode('importFromTsv;ehr;observation_types;/data/observation_types.tsv');
 SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;observation_areas;/data/observation_areas.tsv');
 SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;stool_score;/data/stool_score.tsv');
 SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;editable_lookups;/data/editable_lookups.tsv');
