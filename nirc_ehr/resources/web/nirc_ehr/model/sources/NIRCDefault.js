9239bc40ffb9a949db307178b828d635259b910d
Binal Patel <binalp@labkey.com>
2024-06-18 15:09:05 -0700

    Add comment

--- nirc_ehr/resources/web/nirc_ehr/model/sources/NIRCDefault.js
+++ nirc_ehr/resources/web/nirc_ehr/model/sources/NIRCDefault.js
@@ -20,6 +20,11 @@ EHR.model.DataModelManager.registerMetadata('Default', {
                 store: {
                     type: 'labkey-store',
                     schemaName: 'core',
+
+                    // 'performedby' is a text field and its lookup to the userid is an int field - this mismatch causes it to disappear
+                    // from the display when a value is selected from the dropdown even though the 'userid' value gets saved (this behavior was only seen
+                    // in the form panel but not in the grid panel).
+                    // casting it as a varchar when loading the store fixes this issue.
                     sql: 'SELECT CAST (UserId AS VARCHAR) AS UserId,DisplayName,FirstName,LastName FROM core.users',
                     autoLoad: true
                 },
