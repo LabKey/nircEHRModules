7d18d6389eb669018c5b5774efd5b28b35511656
Binal Patel <binalp@labkey.com>
2024-07-10 18:26:07 -0700

    Metadata updates

--- nirc_ehr/resources/queries/nirc_ehr/necropsyTasks.sql
+++ nirc_ehr/resources/queries/nirc_ehr/necropsyTasks.sql
@@ -1,4 +1,4 @@
-SELECT
+SELECT * FROM (SELECT
     t.taskid,
     d.Id AS animalId,
     t.updateTitle AS form,
@@ -10,7 +10,13 @@ SELECT
     CASE WHEN t.qcstate.Label = 'Request: Pending' THEN 'Death Entered: Necropsy Pending'
          WHEN t.qcstate.Label = 'Review Required' THEN 'Necropsy Entered: Review Required'
         ELSE t.qcstate.Label
-    END AS status
+    END AS status,
+    CASE WHEN t.qcstate.Label = 'Request: Pending' THEN 1
+         WHEN t.qcstate.Label = 'Review Required' THEN 2
+         WHEN t.qcstate.Label = 'Completed' THEN 3
+         ELSE 4
+        END AS statusOrder
 FROM ehr.tasks t
 LEFT JOIN study.deaths d ON d.taskid = t.taskid
-WHERE formType = 'Necropsy'
+WHERE formType = 'Necropsy') n
+ORDER BY n.statusOrder ASC
