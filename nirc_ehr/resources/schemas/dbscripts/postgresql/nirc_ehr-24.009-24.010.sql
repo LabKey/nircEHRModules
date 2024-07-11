275a2c16679216d9f1e17af10d61de4589010baa
Binal Patel <binalp@labkey.com>
2024-07-11 13:30:06 -0700

    Load lookup_sets, editable_lookups

--- nirc_ehr/resources/schemas/dbscripts/postgresql/nirc_ehr-24.009-24.010.sql
+++ nirc_ehr/resources/schemas/dbscripts/postgresql/nirc_ehr-24.009-24.010.sql
@@ -1,5 +1,7 @@
 SELECT core.executeJavaUpgradeCode('reloadFolder');
 
+SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;lookup_sets;/data/lookup_sets.tsv');
+SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;editable_lookups;/data/editable_lookups.tsv');
 SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;death_reason;/data/death_reason.tsv');
 SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;acquisition_type;/data/acquisition_type.tsv');
 SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;housing_reason;/data/housing_reason.tsv');
