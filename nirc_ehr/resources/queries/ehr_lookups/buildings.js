8d1b0cc18dce62c2286f1c143cf387394c0e985b
Marty Pradere <martyp@labkey.com>
2024-08-12 17:45:01 -0700

    Few more fixes

--- nirc_ehr/resources/queries/ehr_lookups/buildings.js
+++ nirc_ehr/resources/queries/ehr_lookups/buildings.js
@@ -2,6 +2,37 @@ var LABKEY = require("labkey");
 
 var triggerHelper = new org.labkey.nirc_ehr.query.NIRC_EHRTriggerHelper(LABKEY.Security.currentUser.id, LABKEY.Security.currentContainer.id);
 
+function onUpsert(row, oldRow, errors){
+    if (extraContext.dataSource != "etl") {
+        if (!row.description) {
+            errors['description'] = 'Building description is required.';
+            return;
+        }
+
+        if (!row.area) {
+            errors['area'] = 'Area is required.';
+            return;
+        }
+
+        if (!row.name) {
+            if (oldRow && oldRow.name && oldRow.name[0]) {
+                row.name = oldRow.name[0];
+                return;
+            }
+
+            row.name = row.description + '-' + row.area;
+        }
+    }
+}
+
+function beforeInsert(row, errors){
+    onUpsert(row, undefined, errors);
+}
+
+function beforeUpdate(row, oldRow, errors){
+    onUpsert(row, oldRow, errors);
+}
+
 function beforeDelete(row, errors) {
     if (extraContext.dataSource != "etl") {
         if (!row.name) {
