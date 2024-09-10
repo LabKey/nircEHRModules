66b6bdb96fce938ed21bfbdbbdb33ee0b8edf239
Marty Pradere <martyp@labkey.com>
2024-09-10 05:00:17 -0700

    Performed by fix up

--- nirc_ehr/resources/web/nirc_ehr/model/sources/NIRCDefault.js
+++ nirc_ehr/resources/web/nirc_ehr/model/sources/NIRCDefault.js
@@ -10,48 +10,23 @@ EHR.model.DataModelManager.registerMetadata('Default', {
             xtype: 'ehr-animalIdUpperField',
         },
         performedby: {
-            allowBlank: true,
-            nullable: true,
-            defaultValue: LABKEY.Security.currentUser.id.toString(),
-            editorConfig: {
-                xtype: 'combo',
-                displayField: 'DisplayName',
-                valueField: 'UserId',
-                forceSelection: true,
-                listConfig: {
-                    innerTpl: '{[LABKEY.Utils.encodeHtml(values.DisplayName + (values.LastName ? " (" + values.LastName + (values.FirstName ? ", " + values.FirstName : "") + ")" : ""))]}',
-                    getInnerTpl: function(){
-                        return this.innerTpl;
-                    }
-                }
+            getInitialValue: function (v, rec) {
+                return LABKEY.Security.currentUser.id;
+            },
+            hidden: false,
+            allowBlank: false,
+        },
+        orderedby: {
+            hidden: false,
+            allowBlank: false,
+            defaultValue: null,
+            columnConfig: {
+                width: 160
             }
         }
     },
     byQuery: {
         'study.housing': {
-            // having to add performedby here for housing because it is getting overridden in EHR's default.js.
-            performedby: {
-                allowBlank: false,
-                lookup: {
-                    schemaName: 'core',
-                    queryName: 'users',
-                    keyColumn: 'UserId',
-                    displayColumn: 'DisplayName'
-                },
-                editorConfig: {
-                    store: {
-                        type: 'labkey-store',
-                        schemaName: 'core',
-
-                        // 'performedby' is a text field in the dataset and its lookup to the userid is an int field - this mismatch causes it to disappear
-                        // from the display when a value is selected from the dropdown even though the 'userid' value gets saved as a text (this behavior was only seen
-                        // in the form panel but not in the grid panel).
-                        // casting it as a varchar when loading the store fixes this issue.
-                        sql: 'SELECT CAST (UserId AS VARCHAR) AS UserId,DisplayName,FirstName,LastName FROM core.users',
-                        autoLoad: true
-                    }
-                }
-            },
             room: {
                 allowBlank: true,
                 hidden: true
@@ -102,77 +77,18 @@ EHR.model.DataModelManager.registerMetadata('Default', {
                 columnConfig: {
                     width: 400
                 }
-            },
-            performedby: {
-                editorConfig: {
-                    store: {
-                        type: 'labkey-store',
-                        schemaName: 'core',
-                        sql: 'SELECT CAST (UserId AS VARCHAR) AS UserId,DisplayName,FirstName,LastName FROM core.users',
-                        autoLoad: true
-                    }
-                },
-                defaultValue: LABKEY.Security.currentUser.id.toString()
-            },
-        },
-        'study.weight': {
-            performedby: {
-                editorConfig: {
-                    store: {
-                        type: 'labkey-store',
-                        schemaName: 'core',
-                        sql: 'SELECT CAST (UserId AS VARCHAR) AS UserId,DisplayName,FirstName,LastName FROM core.users',
-                        autoLoad: true
-                    }
-                },
-                defaultValue: LABKEY.Security.currentUser.id.toString()
-            }
-        },
-        'study.flags': {
-            performedby: {
-                editorConfig: {
-                    store: {
-                        type: 'labkey-store',
-                        schemaName: 'core',
-                        sql: 'SELECT CAST (UserId AS VARCHAR) AS UserId,DisplayName,FirstName,LastName FROM core.users',
-                        autoLoad: true
-                    }
-                },
-                defaultValue: LABKEY.Security.currentUser.id.toString()
             }
         },
         'study.treatment_order': {
             category: {
                 defaultValue: 'Clinical',
                 hidden: true
-            },
-            performedby: {
-                hidden: false,
-                allowBlank: false,
-                defaultValue: null,
-                lookup: {
-                    schemaName: 'ehr_lookups',
-                    queryName: 'veterinarians',
-                    keyColumn: 'UserId',
-                    displayColumn: 'DisplayName'
-                }
-            },
+            }
         },
         'study.drug': {
             category: {
                 defaultValue: 'Clinical',
                 hidden: true
-            },
-            performedby: {
-                hidden: false,
-                allowBlank: false,
-                defaultValue: null,
-                lookup: {
-                    schemaName: 'ehr_lookups',
-                    queryName: 'veterinarians',
-                    keyColumn: 'UserId',
-                    displayColumn: 'DisplayName'
-                }
             }
         },
     }
