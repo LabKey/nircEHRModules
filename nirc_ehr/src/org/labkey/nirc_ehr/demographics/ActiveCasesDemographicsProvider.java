a13187cf1afe05b5e113be941db36103f0f9e4ee
Marty Pradere <martyp@labkey.com>
2024-10-07 17:21:51 -0700

    Cleanup

--- nirc_ehr/src/org/labkey/nirc_ehr/demographics/ActiveCasesDemographicsProvider.java
+++ nirc_ehr/src/org/labkey/nirc_ehr/demographics/ActiveCasesDemographicsProvider.java
@@ -1,18 +1,4 @@
-/*
- * Copyright (c) 2013-2016 LabKey Corporation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.labkey.nirc_ehr.demographics;
 
 import org.labkey.api.data.CompareType;
@@ -76,7 +62,6 @@ public class ActiveCasesDemographicsProvider extends AbstractListDemographicsPro
     @Override
     public boolean requiresRecalc(String schema, String query)
     {
-        return ("study".equalsIgnoreCase(schema) && "cases".equalsIgnoreCase(query)) ||
-                ("study".equalsIgnoreCase(schema) && "problem".equalsIgnoreCase(query));
+        return ("study".equalsIgnoreCase(schema) && "cases".equalsIgnoreCase(query));
     }
 }
