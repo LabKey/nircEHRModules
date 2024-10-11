9a31282eeff36b13826655b3382968e324fa4ed3
Marty Pradere <martyp@labkey.com>
2024-10-11 08:43:49 -0700

    Reorder firing events

--- nirc_ehr/resources/web/nirc_ehr/buttons/SelectCaseButton.js
+++ nirc_ehr/resources/web/nirc_ehr/buttons/SelectCaseButton.js
@@ -12,14 +12,14 @@ Ext4.define('NIRC_EHR.form.field.SelectCaseButton', {
         const store = this.getStore(caseid);
         this.on('storeloaded', function(panel){
 
-            const rec = store.getAt(0);
-            EHR.DemographicsCache.reportCaseSelected(rec);
-            panel.fireEvent('animalchange', rec.data.Id);
-
             let editCases = this.up('#upperPanel')?.query('nirc_ehr-editCases')?.[0];
             editCases.fieldEnableChange(true);
             if (hideEdit)
                 editCases.editBtn.hide();
+
+            const rec = store.getAt(0);
+            EHR.DemographicsCache.reportCaseSelected(rec);
+            panel.fireEvent('animalchange', rec.data.Id);
         });
     },
 
