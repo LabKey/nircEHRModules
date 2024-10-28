9567e21892fa14f50bed889e7464de62306efb8a
Marty Pradere <martyp@labkey.com>
2024-10-28 09:45:36 -0700

    Behavior Cases (#428)
    * Add behavior cases form
    * Add behavior cases reports

---
+++ nirc_ehr/resources/web/nirc_ehr/model/sources/BehavioralCase.js
@@ -0,0 +1,112 @@
+
+EHR.model.DataModelManager.registerMetadata('BehavioralCase', {
+    allQueries: {
+        Id: {
+            inheritFromParent: true,
+            editable: false,
+            hidden: true,
+            columnConfig: {
+                editable: false
+            }
+        },
+        date: {
+            editable: true,
+            hidden: false,
+            columnConfig: {
+                editable: true
+            }
+        },
+        caseid: {
+            inheritFromParent: true,
+            editable: false,
+            hidden: true,
+            columnConfig: {
+                editable: false
+            }
+        }
+    },
+    byQuery: {
+        'study.cases': {
+            Id: {
+                xtype: 'nirc_ehr-animalIdCases',
+                inheritFromParent: false,
+                editable: true,
+                hidden: false,
+                columnConfig: {
+                    editable: true
+                }
+            },
+            category: {
+                getInitialValue: function (v, rec){
+                    return 'Behavior'
+                },
+                editable: false,
+                hidden: true,
+                columnConfig: {
+                    editable: false
+                }
+            },
+            problemCategory: {
+                editorConfig: {
+                    listeners: {
+                        select: function (field, recs) {
+                            if (!recs || recs.length !== 1)
+                                return;
+
+                            var record = EHR.DataEntryUtils.getBoundRecord(field);
+                            if (record) {
+                                var rec = recs[0];
+                                var meta = record.store.model.prototype.fields.get('problemSubcategory');
+                                var storeId = LABKEY.ext4.Util.getLookupStoreId(meta);
+                                var store = Ext4.StoreMgr.get(storeId);
+                                if (store) {
+                                    store.filterArray = [LABKEY.Filter.create('category', rec.get('value'))];
+                                    store.load();
+                                }
+                            }
+                        }
+                    }
+                },
+            },
+            openRemark: {
+                height: 120
+            },
+            plan: {
+                height: 120
+            },
+            closeRemark: {
+                height: 120
+            },
+            qcstate: {
+                hidden: true
+            },
+            openDiagnosis: {
+                hidden: true
+            },
+            closeDiagnosis: {
+                hidden: true
+            },
+            attachmentFile: {
+                hidden: true
+            },
+            caseCategory: {
+                hidden: true
+            },
+            performedby: {
+                hidden: false,
+                defaultValue: LABKEY.Security.currentUser.id.toString(),
+                editorConfig: {
+                    store: {
+                        type: 'labkey-store',
+                        schemaName: 'core',
+                        sql: 'SELECT CAST (UserId AS VARCHAR) AS UserId,DisplayName,FirstName,LastName FROM core.users',
+                        autoLoad: true
+                    }
+                }
+            },
+            formCasesLink: {
+                xtype: 'nirc_ehr-editCases'
+            }
+        }
+    }
+});
