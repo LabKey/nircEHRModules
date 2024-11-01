03175b5e01cb804b053b777caf2286245ea2f476
Marty Pradere <martyp@labkey.com>
2024-11-01 14:20:03 -0700

    wound validation

--- nirc_ehr/resources/queries/study/clinical_observations.js
+++ nirc_ehr/resources/queries/study/clinical_observations.js
@@ -27,6 +27,11 @@ function onUpsert(helper, scriptErrors, row, oldRow) {
             EHR.Server.Utils.addError(scriptErrors, 'remark', "You selected 'No' for 'Verified Id?', please enter Remark", "WARN");
         }
 
+        var yesRemarkRequired = (row.category === "Self Biting Observed" || row.category === "New Injury Observed" || row.category === "Other Stereotopy" || row.category === "Environmental Change" || row.category === "Special Enrichment");
+        if (yesRemarkRequired && row.observation === "Yes" && !row.remark) {
+            EHR.Server.Utils.addError(scriptErrors, 'remark', "You selected 'Yes' for " + row.category + ", please explain in the Remark", "WARN");
+        }
+
         // Handle scheduled observations
         if (!helper.isValidateOnly() && row.scheduledDate) {
             var qc;
