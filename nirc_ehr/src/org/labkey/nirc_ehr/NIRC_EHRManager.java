9737a6361baa939f3e4166343a513592049eb611
Marty Pradere <martyp@labkey.com>
2024-11-05 04:34:10 -0800

    SIB score

--- nirc_ehr/src/org/labkey/nirc_ehr/NIRC_EHRManager.java
+++ nirc_ehr/src/org/labkey/nirc_ehr/NIRC_EHRManager.java
@@ -9,7 +9,9 @@ public class NIRC_EHRManager
     @Queryable
     public static final String DAILY_CLINICAL_OBS_TITLE = "Daily Clinical Observations";
     public static final List<String> DAILY_CLINICAL_OBS = List.of("Activity", "Appetite", "BCS", "Hydration", "Stool", "Verified Id?");
-    public static final String DAILY_CLINICAL_OBS_LIST = String.join(";",NIRC_EHRManager.DAILY_CLINICAL_OBS);
+    public static final String SIB_OBS_TITLE = "SIB Observations";
+    public static final List<String> SIB_OBS = List.of("Environmental Change", "Self Biting Observed", "Other Stereotopy", "New Injury Observed", "Special Enrichment", "Wound Status", "Wound Severity");
+
     private static NIRCOrchardFileGenerator _orchardFileGenerator = new NIRCOrchardFileGenerator();
     public static NIRCOrchardFileGenerator getOrchardFileGenerator()
     {
