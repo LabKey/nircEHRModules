4d49c9397323cecf4ec30cc1662c7c2b0e412129
Marty Pradere <martyp@labkey.com>
2025-02-18 20:31:28 -0800

    Frequency fk refactor

--- nirc_ehr/resources/web/nirc_ehr/buttons/addClinicalObsButton.js
+++ nirc_ehr/resources/web/nirc_ehr/buttons/addClinicalObsButton.js
@@ -2,7 +2,7 @@
 EHR.DataEntryUtils.registerGridButton('NIRC_DAILY_CLINICAL_OBS', function(config){
     return Ext4.Object.merge({
         text: 'Daily Observations',
-        tooltip: 'Click to delete selected rows',
+        tooltip: 'Click to add daily observations',
         handler: function(btn) {
             LABKEY.Query.selectRows({
                 schemaName: 'ehr',
@@ -40,58 +40,39 @@ EHR.DataEntryUtils.registerGridButton('NIRC_DAILY_CLINICAL_OBS', function(config
 EHR.DataEntryUtils.registerGridButton('NIRC_DAILY_CLINICAL_OBS_ORDERS', function(config){
     return Ext4.Object.merge({
         text: 'Daily Observations',
-        tooltip: 'Click to delete selected rows',
+        tooltip: 'Click to add daily observation orders',
         handler: function(btn) {
-            LABKEY.Query.selectRows({
-                schemaName: 'ehr_lookups',
-                queryName: 'treatment_frequency',
-                columns: 'rowid',
-                filterArray: [LABKEY.Filter.create('meaning', 'SID', LABKEY.Filter.Types.EQUAL)],
-                scope: this,
-                success: function (results) {
-                    if (results?.rows?.length > 0) {
-                        const sidRowId = results.rows[0].rowid;
-                        LABKEY.Query.selectRows({
-                            schemaName: 'ehr',
-                            queryName: 'observation_types',
-                            success: function (results) {
-                                var grid = btn.up('gridpanel');
-                                if (results?.rows?.length > 0) {
-                                    for (var i = 0; i < results.rows.length; i++) {
-                                        var row = results.rows[i];
-                                        if (row.value === 'Verified Id?' || row.value === 'Stool' || row.value === 'Activity' ||
-                                                row.value === 'Appetite' || row.value === 'Hydration' || row.value === 'BCS') {
 
-                                            var newRecord = grid.store.createModel({});
-                                            newRecord.set({
-                                                category: row.value,
-                                                frequency: sidRowId
-                                            });
-                                            grid.store.add(newRecord);
-                                        }
-                                    }
-                                }
-                                else {
-                                    Ext4.Msg.alert('Unable to load observation types. Please contact the system administrator.');
-                                }
-                            },
-                            failure: function (error) {
-                                Ext4.Msg.alert('Unable to load observation types. Please contact the system administrator.');
-                                console.log('Error loading observation types: ' + error);
-                            },
-                            scope: this
-                        });
+            LABKEY.Query.selectRows({
+                schemaName: 'ehr',
+                queryName: 'observation_types',
+                success: function (results) {
+                    var grid = btn.up('gridpanel');
+                    if (results?.rows?.length > 0) {
+                        for (var i = 0; i < results.rows.length; i++) {
+                            var row = results.rows[i];
+                            if (row.value === 'Verified Id?' || row.value === 'Stool' || row.value === 'Activity' ||
+                                    row.value === 'Appetite' || row.value === 'Hydration' || row.value === 'BCS') {
+
+                                var newRecord = grid.store.createModel({});
+                                newRecord.set({
+                                    category: row.value,
+                                    frequency: 'SID'
+                                });
+                                grid.store.add(newRecord);
+                            }
+                        }
                     }
                     else {
-                        Ext4.Msg.alert('Unable to load treatment frequencies. Please contact the system administrator.');
+                        Ext4.Msg.alert('Unable to load observation types. Please contact the system administrator.');
                     }
                 },
                 failure: function (error) {
-                    Ext4.Msg.alert('Unable to load treatment frequencies. Please contact the system administrator.');
-                    console.log('Error loading treatment frequencies: ' + error);
-                }
+                    Ext4.Msg.alert('Unable to load observation types. Please contact the system administrator.');
+                    console.log('Error loading observation types: ' + error);
+                },
+                scope: this
             });
-
         }
     }, config);
 });
