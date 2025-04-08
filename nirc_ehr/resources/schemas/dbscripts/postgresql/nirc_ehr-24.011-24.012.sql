34cdb09561cf7130d5f2b47b12d564b8ea64887e
Marty Pradere <martyp@labkey.com>
2025-04-08 15:45:02 -0700

    put back with correct schema

--- nirc_ehr/resources/schemas/dbscripts/postgresql/nirc_ehr-24.011-24.012.sql
+++ nirc_ehr/resources/schemas/dbscripts/postgresql/nirc_ehr-24.011-24.012.sql
@@ -7,5 +7,6 @@ SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;editable_lookups;/
 SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;activity;/data/activity.tsv');
 SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;app_score;/data/app_score.tsv');
 SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;hyd_score;/data/hyd_score.tsv');
+SELECT core.executeJavaUpgradeCode('importFromTsv;ehr;observation_types;/data/observation_types.tsv');
 SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;stool_types;/data/stool_types.tsv');
 SELECT core.executeJavaUpgradeCode('importFromTsv;ehr_lookups;yesno;/data/yesno.tsv');
