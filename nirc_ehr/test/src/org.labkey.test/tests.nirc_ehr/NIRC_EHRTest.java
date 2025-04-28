48784b2bc3728a1d5a0471a4234f6193b7fd0a79
labkey-danield <danield@labkey.com>
2025-04-28 13:25:09 -0700

    Add some waits for updates. Use dates that are farther apart and less likely to collide. Wait for the element.

--- nirc_ehr/test/src/org.labkey.test/tests.nirc_ehr/NIRC_EHRTest.java
+++ nirc_ehr/test/src/org.labkey.test/tests.nirc_ehr/NIRC_EHRTest.java
@@ -810,26 +810,27 @@ public class NIRC_EHRTest extends AbstractGenericEHRTest implements PostgresOnly
         setFormElement(Locator.textarea("openRemark"), "Clinical Case WorkFlow - Test");
         setFormElement(Locator.textarea("plan"), "Case plan");
         setFormElement(Locator.name("Id"), animalId);
-        _helper.getExt4FieldForFormSection("Clinical Case", "Open Date").setValue(LocalDateTime.now().minusDays(1).format(_dateFormat));
+        _helper.getExt4FieldForFormSection("Clinical Case", "Open Date").setValue(LocalDateTime.now().minusDays(2).format(_dateFormat));
         Assert.assertEquals("Performed by is incorrect ", "vet tech fs", getFormElement(Locator.name("performedby")));
 
         //Fill out Clinical Remarks section with Date, Remark
         scrollIntoView(Locator.textarea("remark"));
-        _helper.getExt4FieldForFormSection("Clinical Remarks", "Date").setValue(LocalDateTime.now().minusDays(2).format(_dateFormat));
+        _helper.getExt4FieldForFormSection("Clinical Remarks", "Date").setValue(LocalDateTime.now().minusDays(3).format(_dateFormat));
         _helper.setDataEntryField("remark", "Clinical Remarks - Test");
         if (null == _helper.getExt4FieldForFormSection("Clinical Remarks", "Remark").getValue())
             _helper.setDataEntryField("remark", "Clinical Remarks - Test");
+        sleep(750);
         waitForTextToDisappear("Remark: WARN: Must enter at least one comment");
 
         Ext4GridRef weight = _helper.getExt4GridForFormSection("Weights");
         _helper.addRecordToGrid(weight);
-        weight.setGridCellJS(1, "date", LocalDateTime.now().minusDays(1).format(_dateFormat));
+        weight.setGridCellJS(1, "date", LocalDateTime.now().minusDays(2).format(_dateFormat));
         weight.setGridCell(1, "weight", "6.000");
 
         log("Adding Medications/Treatments Orders");
         Ext4GridRef orderGrid = _helper.getExt4GridForFormSection("Medications/Treatments Orders");
         _helper.addRecordToGrid(orderGrid);
-        orderGrid.setGridCell(1, "date", LocalDateTime.now().minusDays(2).format(_dateFormat));
+        orderGrid.setGridCell(1, "date", LocalDateTime.now().minusDays(3).format(_dateFormat));
         orderGrid.clickDownArrowOnGrid(1, "code");
         orderGrid.setGridCell(1, "code", "Diazepam");
         orderGrid.clickDownArrowOnGrid(1, "frequency");
@@ -839,6 +840,7 @@ public class NIRC_EHRTest extends AbstractGenericEHRTest implements PostgresOnly
         orderGrid.clickDownArrowOnGrid(1, "orderedby");
         orderGrid.setGridCell(1, "orderedby", NIRC_VET_NAME);
         orderGrid.completeEdit();
+        sleep(1_000);
         submitForm("Submit Final", "Finalize Form");
 
         log("Completing today's Medication Schedule");
@@ -863,7 +865,7 @@ public class NIRC_EHRTest extends AbstractGenericEHRTest implements PostgresOnly
         impersonate(NIRC_FULL_SUBMITTER_VET);
 
         //Go to 'Active Clinical Cases'
-        clickAndWait(Locator.linkWithText("Active Clinical Cases"));
+        waitAndClickAndWait(Locator.linkWithText("Active Clinical Cases"));
 
         //Click on 'Case Update' link
         AnimalHistoryPage historyPage = new AnimalHistoryPage<>(getDriver());
