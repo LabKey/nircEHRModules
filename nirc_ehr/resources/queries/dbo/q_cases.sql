1798b68d8a13012ec7400ad1a6cba0bdca1214d7
Binal Patel <binalp@labkey.com>
2025-05-03 14:05:24 -0700

    Use COALESCE instead of nested CASE statement during string concatenation.

--- nirc_ehr/resources/queries/dbo/q_cases.sql
+++ nirc_ehr/resources/queries/dbo/q_cases.sql
@@ -1,5 +1,5 @@
 SELECT * FROM (
-    SELECT anmEvt.ANIMAL_EVENT_ID                                                AS objectid,
+    SELECT anmEvt.ANIMAL_EVENT_ID                                                 as objectid,
           anm.ANIMAL_ID_NUMBER                                                   AS Id,
           CAST(anmEvt.EVENT_DATETIME AS TIMESTAMP)                               AS caseDate,
           CAST(COALESCE(adt.modified, anmEvt.CREATED_DATETIME) AS TIMESTAMP)     AS modified,
@@ -7,22 +7,10 @@ SELECT * FROM (
                WHEN (trim(anmEvt.STAFF_ID.STAFF_FIRST_NAME) IS NULL OR trim(anmEvt.STAFF_ID.STAFF_LAST_NAME) IS NULL)
                    THEN 'unknown'
                ELSE (trim(anmEvt.STAFF_ID.STAFF_FIRST_NAME)
-                   || '|' || trim(anmEvt.STAFF_ID.STAFF_LAST_NAME)) END)         AS performedby,
-           (CASE
-                WHEN anmEvt.EVENT_ID = 1580
-                    THEN 'Open Remark: ' ||
-                         (CASE WHEN anmCmt.TEXT IS NULL THEN 'None' ELSE anmCmt.TEXT END) ||
-                         '; Open Diagnosis: ' ||
-                         (CASE WHEN anmEvt.DIAGNOSIS IS NULL THEN 'None' ELSE anmEvt.DIAGNOSIS END)
-               END)                                                              AS openRemark,
-           (CASE
-                WHEN anmEvt.EVENT_ID = 1677
-                    THEN 'Close Remark: ' ||
-                         (CASE WHEN anmCmt.TEXT IS NULL THEN 'None' ELSE anmCmt.TEXT END) ||
-                         '; Close Diagnosis: ' ||
-                         (CASE WHEN anmEvt.DIAGNOSIS IS NULL THEN 'None' ELSE anmEvt.DIAGNOSIS END)
-               END)                                                              AS closeRemark,
-          COALESCE(dea.deathDate, dep.eventDate)                                 AS enddate,
+                   || '|' || trim(anmEvt.STAFF_ID.STAFF_LAST_NAME)) END)               AS performedby,
+           (CASE WHEN anmEvt.EVENT_ID = 1580 THEN ('Open Remark: ' || COALESCE(anmCmt.TEXT, 'None') || '; Open Diagnosis: ' || COALESCE(anmEvt.DIAGNOSIS, 'None')) ELSE NULL END)      AS openRemark,
+           (CASE WHEN anmEvt.EVENT_ID = 1677 THEN ('Close Remark: ' || COALESCE(anmCmt.TEXT, 'None') || '; Close Diagnosis: ' || COALESCE(anmEvt.DIAGNOSIS, 'None')) ELSE NULL END)      AS closeRemark,
+           COALESCE(dea.deathDate, dep.eventDate)                                 AS enddate,
           anmEvt.EVENT_ID.NAME                                                   AS category,
           CASE
               WHEN anmEvt.ATTACHMENT_PATH IS NOT NULL THEN
