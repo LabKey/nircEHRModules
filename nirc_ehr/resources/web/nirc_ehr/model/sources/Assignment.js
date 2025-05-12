6d752e786fe5cf056a5d6479ddd4958b463d2bb1
Marty Pradere <martyp@labkey.com>
2025-05-12 08:36:03 -0700

    Couple more filters and unhide start/end date

--- nirc_ehr/resources/web/nirc_ehr/model/sources/Assignment.js
+++ nirc_ehr/resources/web/nirc_ehr/model/sources/Assignment.js
@@ -14,7 +14,10 @@ EHR.model.DataModelManager.registerMetadata('Assignment', {
                     schemaName: 'ehr',
                     queryName: 'project',
                     keyColumn: 'project',
-                    columns: 'project,name'
+                    columns: 'project,name',
+                    filterArray: [
+                        LABKEY.Filter.create('isActive', true, LABKEY.Filter.Types.EQUAL),
+                    ]
                 }
             }
         },
@@ -29,6 +32,11 @@ EHR.model.DataModelManager.registerMetadata('Assignment', {
                     fixed: true,
                     width: 150
                 },
+                lookup: {
+                    filterArray: [
+                        LABKEY.Filter.create('isActive', true, LABKEY.Filter.Types.EQUAL),
+                    ]
+                }
             }
         }
     }
