4504a27b882bcc7135797bb25443c5c502320f00
Marty Pradere <martyp@labkey.com>
2025-07-02 21:15:33 -0700

    prevent re-drafting a case

--- nirc_ehr/resources/queries/study/cases.js
+++ nirc_ehr/resources/queries/study/cases.js
@@ -29,8 +29,7 @@ EHR.Server.TriggerManager.registerHandlerForQuery(EHR.Server.TriggerManager.Even
             triggerHelper.closeDailyClinicalObs(row.caseid, row.enddate);
         }
 
-        var reopen = oldRow && oldRow.enddate && !row.enddate;
-        if (!helper.isValidateOnly() && (reopen || helper.getEvent() == 'insert') && row.caseid && row.Id && row.performedby && row.taskid && row.category == 'Clinical') {
+        if (!helper.isValidateOnly()) {
             var qc;
             if (row.QCStateLabel) {
                 qc = EHR.Server.Security.getQCStateByLabel(row.QCStateLabel);
@@ -42,13 +41,37 @@ EHR.Server.TriggerManager.registerHandlerForQuery(EHR.Server.TriggerManager.Even
             if (!qc) {
                 console.error('Unable to find QCState: ' + row.QCState + '/' + row.QCStateLabel);
             }
-            else if ((qc.Label == 'Completed' || qc.Label == 'Review Required') && row.caseid && row.Id && row.performedby && row.taskid && qc) {
-                var ordersInTransaction = helper.getProperty('ordersInTransaction');
-                var oit = [];
-                if (ordersInTransaction && ordersInTransaction.length) {
-                    oit = ordersInTransaction;
+
+            // Don't allow taking an existing non-draft case back to draft
+            if (oldRow && qc.Label == 'In Progress') {
+                var oldQc = EHR.Server.Security.getQCStateByLabel(oldRow.QCStateLabel);
+                if (oldQc.Label != 'In Progress') {
+                    EHR.Server.Utils.addError(errors, null, 'Cannot save a draft copy of a case already opened or in review.', 'ERROR');
                 }
-                triggerHelper.ensureDailyClinicalObservationOrders(row.Id, row.caseid, row.date, row.performedby, qc.RowId, row.taskid, oit);
+            }
+            else {
+                var reopen = oldRow && oldRow.enddate && !row.enddate;
+                if ((reopen || helper.getEvent() == 'insert') && row.caseid && row.Id && row.performedby && row.taskid && row.category == 'Clinical') {
+                    var qc;
+                    if (row.QCStateLabel) {
+                        qc = EHR.Server.Security.getQCStateByLabel(row.QCStateLabel);
+                    }
+                    else if (row.QCState) {
+                        qc = EHR.Server.Security.getQCStateByRowId(row.QCState);
+                    }
+
+                    if (!qc) {
+                        console.error('Unable to find QCState: ' + row.QCState + '/' + row.QCStateLabel);
+                    }
+                    else if ((qc.Label == 'Completed' || qc.Label == 'Review Required') && row.caseid && row.Id && row.performedby && row.taskid && qc) {
+                        var ordersInTransaction = helper.getProperty('ordersInTransaction');
+                        var oit = [];
+                        if (ordersInTransaction && ordersInTransaction.length) {
+                            oit = ordersInTransaction;
+                        }
+                        triggerHelper.ensureDailyClinicalObservationOrders(row.Id, row.caseid, row.date, row.performedby, qc.RowId, row.taskid, oit);
+                    }
+                }
             }
         }
     }
