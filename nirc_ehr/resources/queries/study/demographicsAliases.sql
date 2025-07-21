12e371b519f957db2f5d3b50e948191ee67f1215
Marty Pradere <martyp@labkey.com>
2025-07-21 00:31:57 -0700

    back out update here

--- nirc_ehr/resources/queries/study/demographicsAliases.sql
+++ nirc_ehr/resources/queries/study/demographicsAliases.sql
@@ -3,4 +3,4 @@ SELECT Id,
        category.title as aliasType
 FROM alias
 GROUP BY Id, category.title
-    PIVOT alias BY aliasType IN ('Microchip','Nickname','Old Tattoo','ISIS Stud Book Number')
+    PIVOT alias BY aliasType IN (SELECT title FROM ehr_lookups.alias_category)
