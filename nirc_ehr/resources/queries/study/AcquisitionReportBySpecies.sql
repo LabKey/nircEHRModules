5dd9eb767914d28a1e4b0020694e43b62f79d360
Marty Pradere <martyp@labkey.com>
2025-12-23 07:13:39 -0800

    Acq Report

---
+++ nirc_ehr/resources/queries/study/AcquisitionReportBySpecies.sql
@@ -0,0 +1,13 @@
+PARAMETERS(StartDate TIMESTAMP, EndDate TIMESTAMP, Project VARCHAR)
+
+SELECT s.species, s.project, s.speciesName, COUNT(*) as Total FROM
+    (
+        SELECT
+            species,
+            species.scientific_name AS speciesName,
+            Center_Arrival,
+            project
+        FROM study.AcquisitionReport ar
+        WHERE CAST(COALESCE(StartDate, '1900-01-01') as date) <= ar.Center_Arrival AND CAST(COALESCE(EndDate, curdate()) as date) >= CAST(ar.Center_Arrival as date) AND Project = ar.project
+    ) s
+GROUP BY s.species, s.project, s.speciesName
