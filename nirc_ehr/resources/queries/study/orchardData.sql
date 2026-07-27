f793763efc1c326193ede0184dc1ad112059127f
NIRC-IT <87669815+NIRC-IT@users.noreply.github.com>
2026-07-27 09:44:16 -0500

    Updated source query to pull last location instead of all locations. (#731)
    ## Rationale
    The source query for the orchard file generator pulled the location
    history for an animal instead of the last location that then sent the
    location history to the LIS.
    
    
    ## Changes
    Change housing table join to demographicsLastLocation join.

--- nirc_ehr/resources/queries/study/orchardData.sql
+++ nirc_ehr/resources/queries/study/orchardData.sql
@@ -11,8 +11,8 @@ SELECT
     pa.protocol.title as protocol,
 	InitCap(pa.protocol.InvestigatorId.LastName) || ', ' || InitCap(pa.protocol.InvestigatorId.FirstName) as PI,
 	InitCap(pa.protocol.AuthorId.LastName) || ', ' || InitCap(pa.protocol.AuthorId.FirstName) as Vet,
-    h.cage.cage as cage,
+    dlh.cage.cage as cage,
     d.calculated_status as alive
 FROM demographics d
-LEFT JOIN housing h ON d.Id = h.Id
+LEFT JOIN demographicsLastHousing dlh ON d.Id = dlh.Id
 LEFT JOIN protocolAssignment pa ON d.Id = pa.Id AND pa.endDate is null
